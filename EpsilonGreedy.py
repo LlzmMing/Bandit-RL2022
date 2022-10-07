b661fdac3bb3e08b45773ec0831b3cf656662ab0
ciaozer <842140713@qq.com>
2022-10-07 20:29:24 +0800

    epsilon greedy

---
+++ EpsilonGreedy.py
@@ -0,0 +1,43 @@
+import numpy as np
+from solver import Solver
+
+
+class EpsilonGreedy(Solver):
+    """ Epsilon贪心算法，继承Solver类 """
+
+    def __init__(self, bandit, epsilon=0.01, init_prob=1.0):
+        super(EpsilonGreedy, self).__init__(bandit)
+        self.epsilon = epsilon
+        self.estimates = np.array([init_prob] * self.bandit.K)  # 初始化对所有拉杆的奖励估值
+
+    def run_one_step(self):
+        if np.random.random() < self.epsilon:
+            k = np.random.randint(0, self.bandit.K)  # 随机选择一个拉杆
+        else:
+            k = np.argmax(self.estimates)  # 选择奖励估值最大的拉杆
+        r = self.bandit.step(k)  # 得到本次动作的奖励
+        self.estimates[k] += 1. / (self.counts[k] + 1) * (r - self.estimates[k])
+        return k
+
+
+class DecayingEpsilonGreedy(Solver):
+    """ epsilon取值随时间衰减的Epsilon贪心算法，继承Solver类 """
+
+    def __init__(self, bandit, init_prob=1.0):
+        super(DecayingEpsilonGreedy, self).__init__(bandit)
+        self.estimates = np.array([init_prob] * self.bandit.K)
+        self.total_count = 0
+
+    def run_one_step(self):
+        self.total_count += 1
+        if np.random.random() < 1 / self.total_count:  # epsilon随时间衰减
+            k = np.random.randint(0, self.bandit.K)
+        else:
+            k = np.argmax(self.estimates)
+
+        r = self.bandit.step(k)
+        self.estimates[k] += 1. / (self.counts[k] + 1) * (r - self.estimates[k])
+
+        return k
+
+
